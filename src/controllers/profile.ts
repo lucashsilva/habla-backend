8c3c9b0ba6c79d3ffce595f084d84d3cbb02d709
Lucas Silva <lucashenrique125@gmail.com>
2018-10-08 21:32:30 -0300

    Model definition and swagger docs for profile

--- src/controllers/profile.ts
+++ src/controllers/profile.ts
@@ -3,9 +3,29 @@ import { controller, httpGet, requestParam, BaseHttpController, httpPut, request
 import { inject } from "inversify";
 import { AuthorizeMiddleware } from "../middlewares/authorize";
 import { Profile } from "../models/profile";
+import { ApiOperationGet, ApiOperationPut, ApiPath } from "swagger-express-ts";
 
+@ApiPath({
+    path: "/profiles",
+    name: "Profiles"
+})
 @controller("/profiles", AuthorizeMiddleware)
 export class ProfileController extends BaseHttpController {
+    @ApiOperationGet({
+        path: "/:uid",
+        description: "Get a specific user profile",
+        parameters: {
+           path: {
+               "uid": { description: "User id" }
+           }
+        },
+        responses: {
+            200: { description: "Success", type: "Profile", model: "Profile" },
+            401: { description: "Unauthorized" }, 
+            404: { description: "Not found" }, 
+            500: { description: "Internal Server Error" }
+        }
+    })
     @httpGet("/:uid")
     private async findOne(@requestParam("uid") uid: string, req: express.Request, res: express.Response, next: express.NextFunction): Promise<Profile> {
         try {
@@ -24,6 +44,20 @@ export class ProfileController extends BaseHttpController {
         }
     }
 
+    @ApiOperationPut({
+        path: "/:uid",
+        description: "Update own user profile",
+        parameters: {
+           path: {
+               "uid": { description: "User id" }
+           }
+        },
+        responses: {
+            200: { description: "Success", type: "Post", model: "Post" },
+            401: { description: "Unauthorized" }, 
+            500: { description: "Internal Server Error" }
+        }
+    })
     @httpPut("/:uid")
     private async updateProfile(@requestParam("uid") uid: string, @requestBody() profile: Profile, req: express.Request, res: express.Response, next: express.NextFunction): Promise<Profile> {
         try {
--- src/models/profile.ts
+++ src/models/profile.ts
@@ -1,28 +1,60 @@
 import { Column, Entity, BaseEntity, Index, OneToMany, PrimaryColumn } from "typeorm";
 import { Post } from "./post";
+import { ApiModel, ApiModelProperty } from "swagger-express-ts";
 
+@ApiModel({
+    name: "Profile"
+})
 @Entity()
 export class Profile extends BaseEntity {
+    @ApiModelProperty({
+        description: "User id" ,
+        required: true
+    })
     @PrimaryColumn()
     @Index({ unique: true })
     uid: string;
 
+    @ApiModelProperty({
+        description: "Username" ,
+        required: true
+    })
     @Column()
     @Index({ unique: true })
     username: string;
 
+    @ApiModelProperty({
+        description: "Name",
+        required: true
+    })
     @Column()
     name: string;
 
+    @ApiModelProperty({
+        description: "Bio",
+        required: false
+    })
     @Column({ nullable: true })
     bio: string;
 
+    @ApiModelProperty({
+        description: "Website",
+        required: false
+    })
     @Column({ nullable: true }) 
     website: string;
 
+    @ApiModelProperty({
+        description: "Phone",
+        required: false
+    })
     @Column({ nullable: true })
     phone: string;
     
+    @ApiModelProperty({
+        description: "Gender",
+        required: false
+    })
     @Column({ type: 'enum', enum: ['MALE', 'FEMALE', 'OTHER'], nullable: true })
     gender: 'MALE' | 'FEMALE' | 'OTHER';
 
