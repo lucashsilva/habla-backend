382c5536dd6a6a78edab2fcaf0d138a6984d49d3
Lucas Silva <lucashenrique125@gmail.com>
2019-03-26 20:34:10 -0300

    Fix bug that showed Object as error message

--- src/errors/authentication-error.ts
+++ src/errors/authentication-error.ts
@@ -2,7 +2,7 @@ import HablaErrorCodes from "./error-codes";
 import { HablaError } from "./habla-error";
 
 export class AuthenticationError extends HablaError {
-    constructor(message?) {
+    constructor(message?: string) {
         super(message || 'Resource requires authentication to be accessed.', HablaErrorCodes.AUTHENTICATION_ERROR);
     }
 }
